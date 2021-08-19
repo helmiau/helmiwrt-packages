86048920260587f25de6570d724bf58944145179
Helmi Amirudin <helmilaw@gmail.com>
2021-08-20 03:22:25 +0700

    Add : luci-app-shutdown

---
+++ luci-app-shutdown/luasrc/controller/shutdown.lua
@@ -0,0 +1,8 @@
+module("luci.controller.shutdown", package.seeall)
+function action_poweroff()
+	luci.http.redirect(luci.dispatcher.build_url('admin/status/overview'))
+	luci.sys.poweroff()
+end
+function index()  
+    entry({"admin", "system", "shutdown"}, call("action_poweroff"), "Shutdown", 80).dependent=false	
+end 
