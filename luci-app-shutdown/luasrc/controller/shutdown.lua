cd6685b4fd62ea8489066738f70b4ea59fb7f0a5
Helmi Amirudin <helmilaw@gmail.com>
2021-10-21 17:45:08 +0700

    Update shutdown.lua

--- luci-app-shutdown/luasrc/controller/shutdown.lua
+++ luci-app-shutdown/luasrc/controller/shutdown.lua
@@ -1,7 +1,7 @@
 module("luci.controller.shutdown", package.seeall)
 function action_poweroff()
 	luci.http.redirect(luci.dispatcher.build_url('admin/status/overview'))
-	luci.sys.poweroff()
+	luci.util.exec("/sbin/poweroff")
 end
 function index()  
     entry({"admin", "system", "shutdown"}, call("action_poweroff"), "Shutdown", 80).dependent=false	
