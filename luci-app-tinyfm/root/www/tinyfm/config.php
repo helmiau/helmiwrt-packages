7a50d6894af877438d718616869f3b099a7a52f1
Helmi Amirudin <helmilaw@gmail.com>
2022-06-09 16:54:52 +0700

    tinyfm: fix error file preview such as image and video

--- luci-app-tinyfm/root/www/tinyfm/config.php
+++ luci-app-tinyfm/root/www/tinyfm/config.php
@@ -46,11 +46,11 @@ $default_timezone = 'Etc/UTC'; // UTC
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path = '/.';
+$root_path = '../';
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
-$root_url = '/.';
+$root_url = '';
 
 // Server hostname. Can set manually if wrong
 $http_host = $_SERVER['HTTP_HOST'];
