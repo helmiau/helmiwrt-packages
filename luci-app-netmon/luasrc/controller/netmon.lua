70d98876ce29587e875954d58dec087550f04a62
Helmi Amirudin <helmilaw@gmail.com>
2022-06-09 16:38:33 +0700

    fix netmon not shown on openwrt 21

--- luci-app-netmon/luasrc/controller/netmon.lua
+++ luci-app-netmon/luasrc/controller/netmon.lua
@@ -3,9 +3,7 @@
 module("luci.controller.netmon", package.seeall)
 
 function index()
-	local page = entry({"admin", "status", "netmon"}, alias("admin", "status", "netmon", "status"), _("Network Monitor"), 14)
-	page.dependent = true
-	page.acl_depends = { "luci-app-netmon" }
+	entry({"admin", "status", "netmon"}, alias("admin", "status", "netmon", "status"), _("Network Monitor"), 14).dependent = false
 
 	entry({"admin", "status", "netmon", "status"}, cbi("netmon/status"), _("Status"), 1).leaf = true
 	entry({"admin", "status", "netmon", "config"}, cbi("netmon/config"), _("Configuration"), 2).leaf = true
