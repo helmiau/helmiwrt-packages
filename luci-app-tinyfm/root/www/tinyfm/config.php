5599270c525fda7fe49fa41f196574bff8238475
Helmi Amirudin <helmilaw@gmail.com>
2022-06-15 13:23:10 +0700

    Update config.php

--- luci-app-tinyfm/root/www/tinyfm/config.php
+++ luci-app-tinyfm/root/www/tinyfm/config.php
@@ -42,7 +42,7 @@ $edit_files = true;
 
 // Default timezone for date() and time()
 // Doc - http://php.net/manual/en/timezones.php
-$default_timezone = 'Etc/UTC'; // UTC
+$default_timezone = 'UTC'; // UTC
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
