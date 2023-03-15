9e885154e9fb9ba6920ebf42a4663e939bd6e64a
Helmi Amirudin <helmilaw@gmail.com>
2023-03-15 15:59:31 +0700

    netmonitor: fix some issues, removed error features

--- luci-app-netmon/luasrc/controller/netmon.lua
+++ luci-app-netmon/luasrc/controller/netmon.lua
@@ -7,10 +7,7 @@ function index()
 
 	entry({"admin", "status", "netmon", "status"}, cbi("netmon/status"), _("Status"), 1).leaf = true
 	entry({"admin", "status", "netmon", "config"}, cbi("netmon/config"), _("Configuration"), 2).leaf = true
-	entry({"admin", "status", "netmon", "myip"}, cbi("netmon/myip"), _("My IP Information"), 3).leaf = true
 	-- entry({"admin", "status", "netmon", "ookla"}, cbi("netmon/ookla"), _("Speedtest Ookla"), 4).leaf = true
-	entry({"admin", "status", "netmon", "openspeedtest"}, cbi("netmon/openspeedtest"), _("OpenSpeedTest"), 5).leaf = true
-	entry({"admin", "status", "netmon", "twilio"}, cbi("netmon/twilio"), _("Twilio Test"), 6).leaf = true
-	entry({"admin", "status", "netmon", "atsameip"}, cbi("netmon/atsameip"), _("AtSameIP"), 7).leaf = true
-	entry({"admin", "status", "netmon", "threatcrowd"}, cbi("netmon/threatcrowd"), _("Threat Crowd"), 8).leaf = true
+	entry({"admin", "status", "netmon", "openspeedtest"}, cbi("netmon/openspeedtest"), _("OpenSpeedTest"), 3).leaf = true
+	entry({"admin", "status", "netmon", "twilio"}, cbi("netmon/twilio"), _("Twilio Test"), 4).leaf = true
 end
